9011139c05c98a43520d010848b43e68e6617246
Theinterna <yalvin58@gmail.com>
2024-10-04 23:54:08 -0700

    Rename airbnb img, add nav component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <nav className="flex shadow-md bg-dark-gray">
+            <img src="./airbnb_logo.png" className='w-auto' />
+        </nav>
+    )   
+}
+
+export default Header;
