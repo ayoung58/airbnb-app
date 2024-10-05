0e64c277b1f1d3804b2dd324b33a3b9ab05e4d07
Theinterna <yalvin58@gmail.com>
2024-10-05 00:34:35 -0700

    Add header component with image and background

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <div className="flex bg-[url('./road_bg.png')] bg-no-repeat bg-[right_85%] bg-cover pb-10 pt-20 items-center justify-center">
+            <img src="./header_img.png" className="max-w-xl" />
+        </div>
+    )
+}
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Nav from './components/Nav';
+import Header from './components/Header';
 
 function App() {
   return (
-    <Nav />
+    <div>
+      <Nav />
+      <Header />
+    </div>
   )
 }
  
