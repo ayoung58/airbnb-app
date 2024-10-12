734607da0719770786ab13bca55bee507610e25d
Theinterna <yalvin58@gmail.com>
2024-10-12 13:28:48 -0700

    Minor styling changes for space arrangement

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,16 +9,16 @@ function Card(props) {
     <div className="w-44 text-xs flex-auto flex flex-col relative">
         {
             badgeText && 
-            <div className="absolute top-2 left-2 bg-white pl-5 pr-5 pt-7 pb-7 rounded-sm font-bold">{badgeText}</div>
+            <div className="absolute top-2 left-2 bg-white pl-2 pr-2 pt-2 pb-2 rounded-md font-bold">{badgeText}</div>
         }
         <img 
             src={`../images/${props.coverImg}`} 
             className="w-full rounded-md mb-2" 
         />
         <div className="flex items-center">
-            <img src="../images/star.png" className="h-3" />
-            <span>{props.stats.rating}</span>
-            <span className="text-gray-700">({props.stats.reviewCount}) • </span>
+            <img src="../images/star.png" className="h-3 mr-[0.125rem]" />
+            <span className="mr-1">{props.stats.rating}</span>
+            <span className="text-gray-700 mr-1">({props.stats.reviewCount}) • </span>
             <span className="text-gray-700">{props.location}</span>
         </div>
         <p className="overflow-hidden text-ellipsis">{props.title}</p>
